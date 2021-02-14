a12803506e7e458c2602a746f907fe1bce1f3f86
James <jamess2eagle@gmail.com>
2021-02-14 10:50:57 -0800

    added LG Projects

---
+++ header.js
@@ -0,0 +1,36 @@
+document.write('<nav class="navbar navbar-expand navbar-dark bg-dark">\
+      <a class="navbar-brand" href="index.html">Home</a>\
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+        <ul class="navbar-nav mr-auto" id = "defaultnav">\
+          <li class="nav-item Visualization" >\
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>\
+              <div class="dropdown-menu" aria-labelledby="Drop-down">\
+                <a class="dropdown-item" href="cs50.html">&emsp;<strong>C, Python </strong> - CS50 Projects</a>\
+                <a class="dropdown-item" href="PythonAPI.html">&emsp;<strong>Python, API, Pandas, Matplotlib, </strong> - Weather Analysis</a>\
+                <a class="dropdown-item" href="scraping.html">&emsp;<strong>Python, Flask, BeautifulSoup</strong> - NASA(Mars data) Web Scraping</a>\
+                <a class="dropdown-item" href="got.html">&emsp;<strong>Python, Pandas, Postgresql </strong> - Having Fun with Game of Thrones Data</a>\
+                <a class="dropdown-item" href="covid.html">&#9733; <strong>Python, Pandas, Plotly, Matplotlib </strong> - COVID Outcomes in Washington State</a>\
+                <a class="dropdown-item" href="plotly.html">&emsp;<strong>HTML, CSS, JS, D3, Plotly</strong> - Interactive Biodiversity Dashboard</a>\
+                <a class="dropdown-item" href="d3.html">&emsp;<strong>HTML, CSS, JS, D3</strong> - Interactive Health Demographics Chart</a>\
+                <a class="dropdown-item" href="leaflet.html">&emsp;<strong>HTML, CSS, JS, D3, Leaflet, API</strong> - Interactive Earthquake Map</a>\
+                <a class="dropdown-item" href="weather.html">&#9733; <strong>HTML, CSS, JS, D3, Leaflet, API, Python, Pymongo, Flask, Chart.js</strong> - Weather Warning Map</a>\
+                <a class="dropdown-item" href="exoplanet.html">&#9733; <strong>Python, Sklearn, Tensorflow, Pandas, Joblib</strong> - Machine Learning (Sequential and Linear model) Discovering Exoplanet</a>\
+                <a class="dropdown-item" href="stock.html"> &#9733; <strong>Python, Sklearn, Tensorflow, Numpy, Pandas, Matplotlib, API</strong> - Machine Learning (LSTM model) Predicting Stock Prices</a>\
+                <a class="dropdown-item" href="rl.html"> &#9733; <strong>Python</strong> - Coming soon</a>\
+              </div>\
+          </li>\
+          <li class="nav-item Visualization" >\
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Work Experience</a>\
+            <div class="dropdown-menu" aria-labelledby="Drop-down">\
+              <a class="dropdown-item" href="lg.html">&emsp;<strong>LG Display America, Inc. </strong></a>\
+              <a class="dropdown-item" href="kp.html">&emsp;<strong>Kaiser Permanente </strong> - coming soon</a>\
+            </div>\
+          </li>\
+          <li class="nav-item">\
+            <a class="nav-link" href="contact.html">Contact</a>\
+          </li>\
+        </ul>\
+      </div>\
+    </nav>\
+    <br>\
+');
